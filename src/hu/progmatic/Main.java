a31a2341ea402bc8238d62721257cbe2c5450765
HFKitti <HFK9457@gmail.com>
2022-05-29 19:57:41 +0200

    xy

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,8 +1,44 @@
 package hu.progmatic;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            Scanner scanner = new Scanner(new File("hu/progmatic/receptek "));
+            //System.out.println(sc.contains("tojás"));
+
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
+    //System.out.println(s.contains("tojás"));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
